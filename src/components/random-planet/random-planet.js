fa0b6ba3f018be2ddb1b8fce98b52ed5188a591c
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2020-02-20 11:25:55 +0300

    Base files and components

---
+++ src/components/random-planet/random-planet.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import SwapiService from '../../services/swapi-service';
+
+import './random-planet.css';
+import Spiner from '../spinner';
+import Spinner from '../spinner';
+
+export default class RandomPlanet extends Component {
+
+  SwapiService = new SwapiService();
+
+  state = {
+    planet: {},
+    loading: true
+  };
+
+  constructor() {
+    super();
+    this.updatePlanet();
+  }
+
+  onPlanetLoadet = (planet) => {
+    this.setState({ planet, loading: false });
+  }
+
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 20);
+    this.SwapiService.getPlanet(id).then(this.onPlanetLoadet);
+  }
+
+  render() {
+
+    const { planet, loading } = this.state;
+
+    const spinner = loading ? <Spiner /> : null;
+    const content = !loading ? <PlanetView planet={planet} /> : null;
+
+    return (
+      <div className="random-planet jumbotron rounded">
+        {spinner}
+        {content}
+      </div>
+
+    );
+  }
+}
+
+
+const PlanetView = ({ planet }) => {
+
+  const { id, name, population, rotationPeriod, diameter } = planet;
+
+  return (
+    <React.Fragment>
+      <img className="planet-image" src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+      <div>
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Population</span>
+            <span>{population}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Rotation Period</span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Diameter</span>
+            <span>{diameter}</span>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  )
+} 
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-radio-s05sail04c">
+            <div class="ldio-zccv7lygofj"><div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
